2e6949c39b94dc0927594fafa4ec360483b7459b
Ry Ferguson <rytf12@gmail.com>
2012-06-08 00:50:17 -0700

    add MD5 fallback to getHash() function.

--- cityphp/functions.php
+++ cityphp/functions.php
@@ -4,7 +4,7 @@ function getHash($input, $salt = NULL) {
     if(is_null($salt)) {
         $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ./';
         $numChars = strlen($chars);
-        $salt = '$2a$12$';
+        $salt = CRYPT_BLOWFISH ? '$2a$12$' : '$1$';
 
         for($i = 0; $i < 22; ++$i) {
             $salt .= $chars[mt_rand(0, $numChars - 1)];
