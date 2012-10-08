6e22020e7de9babd0b4b588231253908a1eb1fc5
Ry Ferguson <rytf12@gmail.com>
2012-10-08 00:19:22 -0700

    make form labels block, add remember me label

--- sample_applications/vanilla/vanilla/html/logIn.php
+++ sample_applications/vanilla/vanilla/html/logIn.php
@@ -4,14 +4,21 @@ require_once(VANILLA . 'html/error.php');
 require_once(VANILLA . 'html/input.php');
 
 function login($formData, $error = '') {
-    return sprintf('<form method="post">%s%s%s'
-        . '<div><input type="checkbox" name="rememberme"/>Remember Me</div>'
-        . '<div><a href="%s">forgot password</a></div>'
-        . '<div><input type="submit" value="Log In"/></div></form>',
-        error($error),
-        input('Username', 'username', $formData['username']),
-        input('Password', 'password', $formData['password'], 'password'),
-        FORGOT_PASSWORD);
+    ob_start(); ?>
+
+<form method="post">
+    <?=error($error)?>
+    <?=input('Username', 'username', $formData['username'])?>
+    <?=input('Password', 'password', $formData['password'], 'password')?>
+    <div>
+        <input type="checkbox" id="rememberme" name="rememberme"
+        /><label for="rememberme">Remember Me</label>
+    </div>
+    <div><a href="<?=FORGOT_PASSWORD?>">forgot password</a></div>
+    <div><input type="submit" value="Log In"/></div>
+</form>
+
+    <?php return ob_get_clean();
 }
 
 ?>
