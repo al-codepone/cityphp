bc2d0d0cbf67f589adefe07c3f9b0070135c13b6
Ry Ferguson <rytf12@gmail.com>
2012-11-23 13:41:16 -0800

    refactor autofocus()

--- cityphp/html/autofocus.php
+++ cityphp/html/autofocus.php
@@ -1,8 +1,7 @@
 <?php
 
 function autofocus($id) {
-    return sprintf('<script>document.getElementById'
-        . '("%s").focus();</script>', $id);
+    return "<script>document.getElementById('$id').focus();</script>";
 }
 
 ?>
