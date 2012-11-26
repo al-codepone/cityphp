cbee8ba94d3394fef903c7101f7571385465ea20
Ry Ferguson <rytf12@gmail.com>
2012-11-26 00:57:03 -0800

    refactor forgot password html

--- sample-applications/vanilla/vanilla/html/forgotPassword.php
+++ sample-applications/vanilla/vanilla/html/forgotPassword.php
@@ -4,10 +4,16 @@ require_once(CITYPHP . 'html/error.php');
 require_once(CITYPHP . 'html/input.php');
 
 function forgotPassword($formData, $error = '') {
-    return sprintf('<form method="post">%s%s'
-        . '<div><input type="submit" value="Submit"/></div></form>',
-        error($error),
-        input('Email', 'email', $formData['email'], 'email'));
+    ob_start(); ?>
+
+<form method="post">
+    <div>Submit your account email and we'll send you directions for resetting your password.</div>
+    <?=error($error)?>
+    <?=input('Email', 'email', $formData['email'], 'email')?>
+    <div><input type="submit" value="Submit"/></div>
+</form>
+
+    <?php return ob_get_clean();
 }
 
 ?>
