992e5c107b40ba29179b137c1ad84b773e061195
Ry Ferguson <rytf12@gmail.com>
2012-11-27 00:42:29 -0800

    refactor settings html, add settings directions

--- sample-applications/vanilla/vanilla/html/settings.php
+++ sample-applications/vanilla/vanilla/html/settings.php
@@ -8,14 +8,23 @@ function settings($formData, $error = '') {
 
 <form method="post" id="settings_form">
     <input type="hidden" name="delete_flag" value="0"/>
+    <div>
+        Use this form to edit your account info.
+        Email and new password are optional.
+        You can delete your account email by submitting a blank email.
+        If you change your email, then your account email will be blank
+        until you verify the new email.
+    </div>
     <?=error($error)?>
     <?=input('Username', 'username', $formData['username'])?>
     <?=input('Email', 'email', $formData['email'], 'email')?>
     <?=input('New Password', 'password', $formData['password'], 'password')?>
     <?=input('Confirm New Password', 'confirm_password', $formData['confirm_password'], 'password')?>
     <?=input('Current Password', 'current_password', $formData['current_password'], 'password')?>
-    <div><input type="submit" value="Submit"/></div>
-    <div><input type="button" value="Delete Account" onClick="deleteAccount();"/></div>
+    <div>
+        <input type="submit" value="Submit"
+        /><input type="button" value="Delete Account" onClick="deleteAccount();"/>
+    </div>
 </form>
 
     <?php return ob_get_clean();
