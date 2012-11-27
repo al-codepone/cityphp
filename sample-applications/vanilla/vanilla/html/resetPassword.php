f24075f78bd7e4de47c79769a85c12717847a8c4
Ry Ferguson <rytf12@gmail.com>
2012-11-27 01:20:03 -0800

    refactor reset password html

--- sample-applications/vanilla/vanilla/html/resetPassword.php
+++ sample-applications/vanilla/vanilla/html/resetPassword.php
@@ -4,14 +4,17 @@ require_once(CITYPHP . 'html/error.php');
 require_once(CITYPHP . 'html/input.php');
 
 function resetPassword($formData, $error = '') {
-    return sprintf('<form method="post">%s%s%s'
-        . '<div><input type="submit" value="Reset"/></div></form>',
-        error($error),
-        input('New Password', 'password', $formData['password'], 'password'),
-        input('Confirm New Password',
-            'confirm_password',
-            $formData['confirm_password'],
-            'password'));
+    ob_start(); ?>
+
+<form method="post">
+    <div>Use this form to reset your password.</div>
+    <?=error($error)?>
+    <?=input('New Password', 'password', $formData['password'], 'password')?>
+    <?=input('Confirm New Password', 'confirm_password', $formData['confirm_password'], 'password')?>
+    <div><input type="submit" value="Reset"/></div>
+</form>
+
+    <? return ob_get_clean();
 }
 
 ?>
