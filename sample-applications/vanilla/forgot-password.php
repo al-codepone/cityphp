e38d05879b168ee4fd38cb45f8c0541545695619
Ry Ferguson <rytf12@gmail.com>
2012-12-28 00:46:45 -0800

    change forgot password submit message

--- sample-applications/vanilla/forgot-password.php
+++ sample-applications/vanilla/forgot-password.php
@@ -12,9 +12,12 @@ if(list($formData, $errors) = $validator->validate()) {
     }
     else {
         $resetPasswordModel = ModelFactory::get('ResetPasswordModel');
-        $content = ($error = $resetPasswordModel->createToken($formData['email']))
-            ? forgotPassword($formData, $error)
-            : 'We emailed you directions for resetting your password.';
+        $resetPasswordModel->createToken($formData['email']);
+        $content = sprintf("If the email address you entered, %s,
+            is associated with a user account, then you will receive
+            an email with directions for resetting your password. If
+            you don't receive this email then please check your junk mail folder.",
+            $formData['email']);
     }
 }
 else {
--- sample-applications/vanilla/vanilla/database/ResetPasswordModel.php
+++ sample-applications/vanilla/vanilla/database/ResetPasswordModel.php
@@ -12,18 +12,16 @@ class ResetPasswordModel extends TokenModel {
         $userModel = ModelFactory::get('UserModel');
         $userData = $userModel->getUserWithEmail($email);
 
-        if(!$userData) {
-            return "Email \"$email\" doesn't exist";
+        if($userData) {
+            $token = sha1Token();
+            $subject = 'Reset Your Password';
+            $additionalHeaders = sprintf("From: %s\r\n", EMAIL_FROM);
+            $message = sprintf("%s,\n\nUse this link to reset your password:\n\n%s%s%d/%s",
+                $userData['username'], SITE, RESET_PASSWORD, $userData['user_id'], $token);
+
+            parent::createToken($userData['user_id'], $token);
+            mail($email, $subject, $message, $additionalHeaders);
         }
-
-        $token = sha1Token();
-        $subject = 'Reset Your Password';
-        $additionalHeaders = sprintf("From: %s\r\n", EMAIL_FROM);
-        $message = sprintf("%s,\n\nUse this link to reset your password:\n\n%s%s%d/%s",
-            $userData['username'], SITE, RESET_PASSWORD, $userData['user_id'], $token);
-
-        parent::createToken($userData['user_id'], $token);
-        mail($email, $subject, $message, $additionalHeaders);
     }
 
     public function getToken($userID, $token) {
