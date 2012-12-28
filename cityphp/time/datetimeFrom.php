8831a605632757ddc08cfc76d91660be95199692
Ry Ferguson <rytf12@gmail.com>
2012-12-27 18:18:29 -0800

    remove datetimeFrom()

--- cityphp/time/datetimeFrom.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function datetimeFrom($datetime, $timezone) {
-    $date = new DateTime($datetime, new DateTimeZone($timezone)); 
-    date_default_timezone_set('UTC'); 
-    return date('Y-m-d H:i:s', $date->format('U'));
-}
-
-?>
