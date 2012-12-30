9d69cf20ffb99ade0195ebe55c57234f6f48bdf4
Ry Ferguson <rytf12@gmail.com>
2012-12-29 16:05:37 -0800

    fix username error typo

--- sample-applications/vanilla/deploy/vanilla/validateUsername.php
+++ sample-applications/vanilla/deploy/vanilla/validateUsername.php
@@ -2,7 +2,7 @@
 
 function validateUsername($value) {
     if(!preg_match('/^[a-z0-9-]{4,16}$/i', $value)) {
-        return 'Username must me 4-16 characters and use letters, numbers and dashes only';
+        return 'Username must be 4-16 characters and use letters, numbers and dashes only';
     }
 }
 
