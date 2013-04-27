20d22566e0e9069057834752f5bf7282a4888e23
Ry Ferguson <rytf12@gmail.com>
2013-04-27 02:14:19 -0700

    remove concat

--- sample-applications/vanilla/deploy/vanilla/routes/sign-up.php
+++ sample-applications/vanilla/deploy/vanilla/routes/sign-up.php
@@ -17,8 +17,8 @@ else if(list($formData, $errors) = $validator->validate()) {
         $content = signUp($formData, $error);
     }
     else {
-        $content = sprintf('Thank you for signing up.'
-            . ' You can now <a href="%s">log in</a>. %s',
+        $content = sprintf('Thank you for signing up.
+            You can now <a href="%s">log in</a>. %s',
             LOGIN, $formData['email']
                 ? 'We emailed you a link to verify your email.'
                 : 'You can assign an email to your account on the edit account page.');
