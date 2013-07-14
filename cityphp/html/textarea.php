d87fbe4bb68911f1b25b528583f8a27b547872c9
Ry Ferguson <rytf12@gmail.com>
2013-07-13 22:46:59 -0700

    upgrade html textarea

--- cityphp/html/textarea.php
+++ cityphp/html/textarea.php
@@ -1,9 +1,28 @@
 <?php
 
-function textarea($label, $id, $value) {
-    return sprintf("<div><label for='$id'>$label</label>"
-        . "<textarea name='$id' id='$id'>%s</textarea></div>",
+function textarea($label, $id, $value = '',
+    $attributes = array(), $isContainer = true)
+{
+    ob_start();
+    print $isContainer ? "<div id=\"c_$id\">" : '';
+    print ($label == '')
+        ? ''
+        : sprintf("<label id=\"l_$id\" for=\"$id\">%s</label>",
+            htmlspecialchars($label));
+
+    print '<textarea ';
+
+    foreach($attributes as $i => $v) {
+        print is_int($i)
+            ? "$v "
+            : "$i=\"$v\" ";
+    }
+
+    printf("name=\"$id\" id=\"$id\">%s</textarea>",
         htmlspecialchars($value));
+
+    print $isContainer ? '</div>' : '';
+    return ob_get_clean();
 }
 
 ?>
