12f7fa07b7efa36963206cfb1cfb01c442de38b8
Ry Ferguson <rytf12@gmail.com>
2013-10-24 19:00:09 -0700

    remove leading slash from autoload class name

--- cityphp/__autoload.php
+++ cityphp/__autoload.php
@@ -1,7 +1,9 @@
 <?php
 
 function __autoload($className) {
-    require_once VENDOR . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+    require_once VENDOR
+        . str_replace('\\', DIRECTORY_SEPARATOR, ltrim($className, '\\'))
+        . '.php';
 }
 
 ?>
