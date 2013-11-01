50e8ffe76938dbd59e6b9e2c316e7a3f32841dd0
Ry Ferguson <rytf12@gmail.com>
2013-11-01 01:04:12 -0700

    don't show empty string attributes

--- cityphp/html/attributes.php
+++ cityphp/html/attributes.php
@@ -4,9 +4,11 @@ function attributes($attributes = array()) {
     ob_start();
 
     foreach($attributes as $i => $v) {
-        print is_int($i)
-            ? " $v"
-            : sprintf(" $i=\"%s\"", htmlspecialchars($v));
+        if($v !== '') {
+            print is_int($i)
+                ? " $v"
+                : sprintf(" $i=\"%s\"", htmlspecialchars($v));
+        }
     }
 
     return ob_get_clean();
