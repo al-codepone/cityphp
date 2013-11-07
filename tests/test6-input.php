77988429caee21a04b7f549abb8ba55e3baae99b
Ry Ferguson <rytf12@gmail.com>
2013-11-06 22:07:35 -0800

    add test for input()

---
+++ tests/test6-input.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'constants.php';
+require_once CITYPHP . 'html/input.php';
+
+echo implode("\n\n", array(
+    input(),
+
+    input(array('id' => 'username')),
+
+    input(array('id' => 'email'), 'Email'),
+
+    input(
+        array('id' => 'city'),
+        'City',
+        false,
+        array('style' => 'color:pink;')),
+ 
+    input(
+        array('name' => 'zip'),
+        'Zip Code',
+        true,
+        array('style' => 'color:pink;'),
+        array(
+            'style' => 'background:#000000;',
+            'onclick' => 'alert("container");'))));
+
+?>
