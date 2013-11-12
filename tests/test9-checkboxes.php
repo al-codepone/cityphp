7daaab587e47b6674fea7bbfa645468d94d3b16e
Ry Ferguson <rytf12@gmail.com>
2013-11-12 01:53:45 -0800

    add checkboxes() tests

---
+++ tests/test9-checkboxes.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'constants.php';
+require_once CITYPHP . 'html/checkboxes.php';
+
+echo implode("\n\n", array(
+    checkboxes(
+        array('apple', 'pear', 'kiwi'),
+        'fruit'),
+
+    checkboxes(
+        array(1 => 'html', 'css', 'php', 'c++'),
+        'skills',
+        array(1, 4),
+        'Skills'),
+
+    checkboxes(
+        array('circle', 'square', 'triangle'),
+        'shapes',
+        array(),
+        'Shapes',
+        true,
+        array('style' => 'color:green;'))));
+
+?>
