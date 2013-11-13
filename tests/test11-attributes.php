43bfe4425f66e698b8be612bea2bc2dcd58ee86d
Ry Ferguson <rytf12@gmail.com>
2013-11-13 02:42:36 -0800

    add attributes() tests

---
+++ tests/test11-attributes.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'constants.php';
+require_once CITYPHP . 'html/attributes.php';
+
+echo implode("\n\n", array(
+    sprintf('<tag%s/>', attributes(array(
+        'name1' => 'value1',
+        'name2' => '"value2"',
+        'value3'))),
+
+    sprintf('<tag%s></tag>', attributes(array(
+        'name1' => 'value1',
+        'name2' => ''))),
+
+    sprintf('<tag%s/>', attributes(array(
+        '<value1>',
+        false ? 'value2' : '',
+        true ? 'value3' : '')))));
+
+?>
