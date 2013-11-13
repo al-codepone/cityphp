61d7abc9f139a35203db14e083e18d34e56f4ee8
Ry Ferguson <rytf12@gmail.com>
2013-11-12 20:14:39 -0800

    add radioButtons() tests

---
+++ tests/test10-radio-buttons.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once 'constants.php';
+require_once CITYPHP . 'html/radioButtons.php';
+
+echo implode("\n\n", array(
+    radioButtons(
+        array('all', 'active', 'none'),
+        'mode'),
+
+    radioButtons(
+        array('a' => 'buzz', 'b' => 'dreads', 'c' => 'rat tail'),
+        'hair',
+        'b',
+        'Hair Style'),
+
+    radioButtons(
+        array(1 => 'breakfast', 'lunch', 'dinner'),
+        'meal',
+        3,
+        'Favorite Meal',
+        true,
+        array('style' => 'color:gray;'))));
+
+?>
