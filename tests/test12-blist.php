ec8176a9ecc3379538148f9c9de9283fc1ffc6a8
Ry Ferguson <rytf12@gmail.com>
2013-11-16 00:10:45 -0800

    add blist() tests

---
+++ tests/test12-blist.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once 'constants.php';
+require_once CITYPHP . 'html/blist.php';
+
+echo implode("\n\n", array(
+    blist('jan'),
+
+    blist(array('feb', 'mar', 'apr')),
+
+    blist(array(
+        'may' => array('style' => 'font-size:3em;'),
+        'jun',
+        'jul' => array('id' => 'last-item'))),
+
+    blist(array('fall', 'winter', 'spring'),
+        array('class' => 'seasons')),
+
+    blist(array('castle', 'ship', 'forest'),
+        array('type' => 'A'),
+        true)));
+
+?>
