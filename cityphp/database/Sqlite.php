277c7a20ee820cf65f2ed5a9de7d666417374e08
Ry Ferguson <rytf12@gmail.com>
2013-11-23 21:19:00 -0800

    add SQLite support

---
+++ cityphp/database/Sqlite.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace cityphp\database;
+
+class Sqlite extends DatabaseHandle {
+    public function __construct(
+        $filename,
+        $flags = 6,
+        $encryptionKey = '',
+        $errorMessage = 'database error',
+        $debug = false)
+    {
+        $conn = new \SQLite3($filename, $flags, $encryptionKey);
+        parent::__construct($errorMessage, $debug, $conn);
+    }
+
+    public function query($query) {
+        if(!$this->getConn()->exec($query)) {
+            $this->error();
+        }
+    }
+
+    public function fetchQuery($query) {
+        if($result = $this->getConn()->query($query)) {
+            $rows = array();
+
+            while($row = $result->fetchArray(SQLITE3_ASSOC)) {
+                $rows[] = $row;
+            }
+
+            return $rows;
+        }
+
+        $this->error();
+    }
+
+    public function esc($string) {
+        return \SQLite3::escapeString($string);
+    }
+
+    protected function connError() {
+        return $this->getConn()->lastErrorMsg();
+    }
+}
+
+?>
