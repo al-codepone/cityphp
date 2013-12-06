88aa9ef98c4bcbd678abc8fbc9c138acff148765
Ry Ferguson <rytf12@gmail.com>
2013-12-06 02:45:33 -0800

    add datetime database test

--- tests/const.php
+++ tests/const.php
@@ -21,6 +21,7 @@ define('PGSQL_CONN_STRING', 'dbname=example2 user=postgres password=postgres');
 define('SQLITE_FILENAME', 'mydb.db');
 define('DB_DEBUG', true);
 
+define('TABLE_EVENTS', 'events');
 define('TABLE_WORDS', 'words');
 
 ?>
---
+++ tests/test29-datetime-database.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once 'const.php';
+require_once CITYPHP . '__autoload.php';
+require_once CITYPHP . 'time/datetimeNow.php';
+require_once CITYPHP . 'time/datetimeTo.php';
+
+use cityphp\db\Mysql;
+ 
+$db = new MySql(
+    MYSQL_HOST,
+    MYSQL_USERNAME,
+    MYSQL_PASSWORD,
+    MYSQL_DBNAME);
+ 
+$db->exec('CREATE TABLE ' . TABLE_EVENTS . ' (
+    name VARCHAR(32),
+    time DATETIME)');
+
+//use datetimeNow() to store the current time
+$db->exec(sprintf('INSERT INTO %1$s (name, time) VALUES
+    ("one", "%2$s"),
+    ("two", "%2$s" - INTERVAL 2 MINUTE),
+    ("three", "%2$s" - INTERVAL 2 DAY)',
+    TABLE_EVENTS,
+    datetimeNow()));
+
+//use datetimeTo() to convert the stored time
+echo implode('<br/>', array_map(
+    function($event) {
+        return sprintf('%s = %s',
+            htmlspecialchars($event['name']),
+            datetimeTo($event['time'], 'America/Los_Angeles', 'M j, Y g:ia'));
+    },
+    $db->query('SELECT * FROM ' . TABLE_EVENTS)));
+
+?>
